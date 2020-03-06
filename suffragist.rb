35a5872e1ca190b9beafb5815cf373ad42f350c3
Ubuntu <ubuntu@ip-172-31-6-146.us-east-2.compute.internal>
2020-03-06 12:55:11 +0000

    Folder structure

---
+++ suffragist.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+get '/' do
+   @title = 'Welcome to the Suffragist!'
+   erb :index
+end
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+  }
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  erb :cast
+end
